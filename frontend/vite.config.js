904923e4c1e9f0fe5f9443c1c8d0f46f712d000e
TuciTheGreat <martin.iv.dm@gmail.com>
2025-04-14 11:26:00 +0300

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "https://cardnexus-api.onrender.com",
-      "/uploads/": "https://cardnexus-api.onrender.com",
+      "/api/": "http://localhost:5000",
+      "/uploads/": "http://localhost:5000",
     },
   },
 })
