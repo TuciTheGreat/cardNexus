ea72b899c4ed7fb38e11b8d3d8021d91109a8dd9
TuciTheGreat <martin.iv.dm@gmail.com>
2025-05-02 16:16:56 +0300

    new changes

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-
+ 
 //Utiles
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -11,30 +11,36 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-
-dotenv.config({path: "./backend/.env"});
+ 
+dotenv.config({ path: "./backend/.env" });
 const port = process.env.PORT || 5000;
-
+ 
 connectDB();
-
+ 
 const app = express();
-
+ 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-
+ 
+const __dirname = path.resolve();
+ 
+app.use(express.static(path.resolve(__dirname, "./backend/client/dist")));
+ 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
-
-
-app.get("/api/config/paypal", (req, res) => {
-    res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
-});
-
-const __dirname = path.resolve();
+ 
 app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
-
+ 
+app.get("/api/config/paypal", (req, res) => {
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
+});
+ 
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./backend/client/dist", "index.html"));
+});
+ 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
